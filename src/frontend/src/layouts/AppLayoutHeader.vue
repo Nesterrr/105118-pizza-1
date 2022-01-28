20d44aa7488b02810184c863851a28f11465e719
Svyatoslav Nesteruk <SINesteruk@sberbank.ru>
2022-01-28 16:07:15 +0300

    module3-task1 add routes: /cart, /orders, /profile

--- src/frontend/src/layouts/AppLayoutHeader.vue
+++ src/frontend/src/layouts/AppLayoutHeader.vue
@@ -1,27 +1,52 @@
 <template>
   <header class="header">
     <div class="header__logo">
-      <a href="index.html" class="logo">
+      <router-link to="/" class="logo">
         <img
           src="@/assets/img/logo.svg"
           alt="V!U!E! Pizza logo"
           width="90"
           height="40"
         />
-      </a>
+      </router-link>
     </div>
     <div class="header__cart">
-      <a href="cart.html">0 ₽</a>
+      <router-link to="/cart">0 ₽</router-link>
     </div>
-    <div class="header__user">
+    <div v-if="false" class="header__user">
+      <router-link to="/profile">
+        <picture>
+          <source
+            type="image/webp"
+            srcset="
+              @/assets/img/users/user5.webp    1x,
+              @/assets/img/users/user5@2x.webp 2x
+            "
+          />
+          <img
+            src="@/assets/img/users/user5.jpg"
+            srcset="@/assets/img/users/user5@2x.jpg"
+            alt="Василий Ложкин"
+            width="32"
+            height="32"
+          />
+        </picture>
+        <span>Василий Ложкин</span>
+      </router-link>
+      <router-link to="/" @click.native.self="logout" class="header__logout">
+        <span>Выйти</span>
+      </router-link>
+    </div>
+    <div v-else class="header__user">
       <router-link to="/login" class="header__login"
-        ><span>Войти</span></router-link
-      >
+        ><span>Войти</span>
+      </router-link>
     </div>
     <router-view />
   </header>
 </template>
 <script>
+// нормально ли что <router-view /> в layout-e?
 export default {
   name: "AppLayoutHeader",
 };
--- src/frontend/src/router/routes.js
+++ src/frontend/src/router/routes.js
@@ -9,20 +9,26 @@ export default [
         path: "/login",
         name: "Login",
         component: () => import("../views/Login.vue"),
-        meta: { showModal: true, layout: "AppLayoutMain" },
+        meta: { layout: "AppLayoutMain" },
       },
     ],
   },
-  // {
-  //   path: '/tasks/create',
-  //   name: 'TaskCreate',
-  //   component: () => import('../views/TaskCreate.vue'),
-  //   meta: { layout: 'AppLayoutDefault' }
-  // },
-  // {
-  //   path: '/tasks/edit/:id',
-  //   name: 'TaskEdit',
-  //   component: () => import('../views/TaskEdit.vue'),
-  //   meta: { layout: 'AppLayoutDefault' }
-  // }
+  {
+    path: "/cart",
+    name: "Cart",
+    component: () => import("../views/Cart.vue"),
+    meta: { layout: "AppLayoutMain" },
+  },
+  {
+    path: "/orders",
+    name: "Orders",
+    component: () => import("../views/Orders.vue"),
+    meta: { layout: "AppLayoutMain" },
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: () => import("../views/Profile.vue"),
+    meta: { layout: "AppLayoutMain" },
+  },
 ];
