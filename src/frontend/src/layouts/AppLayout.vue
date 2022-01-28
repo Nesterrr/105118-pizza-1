59093718ab1d0a17a1df9dbbfd45ebe1b0034918
Svyatoslav Nesteruk <SINesteruk@sberbank.ru>
2022-01-28 16:19:07 +0300

    module3-task1 delete comments

--- src/frontend/src/layouts/AppLayout.vue
+++ src/frontend/src/layouts/AppLayout.vue
@@ -12,16 +12,6 @@ export default {
       const layout = this.$route.meta.layout || defaultLayout;
       return () => import(`@/layouts/${layout}.vue`);
     },
-    // modal() {
-    //   const hasLoginModal = this.$route.meta.hasModal || false;
-    //   return () => import (`@/layouts/${layout}.vue`);
-    // },
-    // watch: {
-    //   $route(newVal) {
-    //     console.log(newVal);
-    //     this.showModal = newVal.meta?.showModal;
-    //   },
-    // },
   },
 };
 </script>
